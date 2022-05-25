d126edb17c3e104aea4c38bb223107412e5d64cf
ratherblue <ratherblue@gmail.com>
2022-05-25 14:32:12 -0700

    linting config pass one

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended'
+  ],
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
+  overrides: [
+    {
+      files: ['cypress/integration/**.spec.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended'],
+    },
+  ],
+  rules: {
+    semi: 'off',
+    '@typescript-eslint/semi': ['error', 'never'],
+    quotes: 'off',
+    '@typescript-eslint/quotes': ['error', 'single'],
+  },
+}
